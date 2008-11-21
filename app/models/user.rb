59cce940960ec1632d0ffec78effb831e3c15309
Richard Hurt <rnhurt@kangaroobox.com>
2008-11-21 13:06:06 -0500

    user table is complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,23 @@ class User < ActiveRecord::Base
 	validates_presence_of :last_name, :message => " is required"
 	validates_existence_of :site
 
+	## Search for a user (will_paginate)
 	def self.search(search, page)
 		paginate :per_page => 10, :page => page,
 							:conditions => ['first_name like ? or last_name like ?', "%#{search}%", "%#{search}%"], 
-							:order => 'created_at DESC'
+							:order => 'first_name'
 	end
 		
+	## Display the users full name
+	def full_name
+		[first_name, last_name].join(' ')
+	end
+	
+	## Break a full name into first_name & last_name
+	def full_name=(name)
+		split = name.split(' ', 2)
+		self.first_name = split.first
+		self.last_name = split.last
+	end
+
 end
