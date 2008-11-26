892078e9c07ebbf161910459dc2c1f12d8c40c16
Richard Hurt <rnhurt@kangaroobox.com>
2008-11-26 09:46:29 -0500

    working on assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -17,15 +17,16 @@ class AssignmentsController < ApplicationController
 													:conditions => { :course_id => params[:id]})
 
 	  respond_to do |format|
-	    format.html # show.html.erb
-	    format.xml  { render :xml => @assignment }
-	    format.js		{ render :partial => "assignment_list" }
+	    format.html { redirect_to assignments_url } 					# Dont show an individual assignment
+	    format.js		{ render :partial => "assignment_list" }	# Render the list of assignments for a course
+	    format.xml  { render :xml => @assignment }						# default
 	  end
   end
 
 
   def new
     @assignment = Assignment.new
+    @course = params[:id]
 
     respond_to do |format|
       format.html # new.html.erb
@@ -45,7 +46,7 @@ class AssignmentsController < ApplicationController
     respond_to do |format|
       if @assignment.save
         flash[:notice] = 'Assignment was successfully created.'
-        format.html { redirect_to(@assignment) }
+        format.html { redirect_to(assignments_url) }
         format.xml  { render :xml => @assignment, :status => :created, :location => @assignment }
       else
         format.html { render :action => "new" }
