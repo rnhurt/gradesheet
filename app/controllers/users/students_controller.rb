65ada5c01979b2ae68b0becadf0c75f98df00a28
Richard Hurt <rnhurt@kangaroobox.com>
2008-12-05 15:03:36 -0500

    woohoo.  users/students|teachers|teachersassistants is working\!

---
+++ app/controllers/users/students_controller.rb
@@ -0,0 +1,82 @@
+class Users::StudentsController < ApplicationController
+	layout "standard"
+
+  def index
+    @students = Student.search(params[:search], params[:page])
+    @types = User.find_user_types(:all)
+
+    respond_to do |format|
+      format.html
+      format.js { render :partial => "users/user_list", :locals => { :users => @students }}
+    end
+  end
+
+
+  def show
+    @student = Student.find(params[:id], :include => :site)
+
+    respond_to do |format|
+      format.html { redirect_to users_path }
+    end
+  end
+
+
+  def new
+    @student = Student.new
+
+    respond_to do |format|
+      format.html { redirect_to users_path }
+    end
+  end
+
+
+  def edit
+    @student = Student.find(params[:id])
+    
+    respond_to do |format|
+    	format.html	{ redirect_to users_path }
+    end
+  end
+
+
+  def create
+    @student = Student.new(params[:student])
+
+    respond_to do |format|
+      if @student.save
+        flash[:notice] = 'Student was successfully created.'
+        format.html { redirect_to(@student) }
+        format.xml  { render :xml => @student, :status => :created, :location => @student }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @student.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def update
+    @student = Student.find(params[:id])
+
+    respond_to do |format|
+      if @student.update_attributes(params[:student])
+        flash[:notice] = 'Student was successfully updated.'
+        format.html { redirect_to(students_url) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @student.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(students_url) }
+    end
+  end
+end
---
+++ app/controllers/users/teachers_controller.rb
@@ -0,0 +1,79 @@
+class Users::TeachersController < ApplicationController
+	layout "standard"
+	
+  def index
+    @teachers = Teacher.search(params[:search], params[:page])
+    @types = User.find_user_types(:all)
+
+    respond_to do |format|
+      format.html
+      format.js { render :partial => "users/user_list", :locals => { :users => @teachers } }
+    end
+  end
+
+
+  def show
+   	@teacher = Teacher.find(params[:id], :include => :site)
+
+    respond_to do |format|
+      format.html { redirect_to users_path }
+    end
+  end
+
+
+  def new
+    @teacher = Teacher.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
+
+
+  def edit
+    @teacher = Teacher.find(params[:id])
+  end
+
+
+  def create
+    @teacher = Teacher.new(params[:teacher])
+
+    respond_to do |format|
+      if @teacher.save
+        flash[:notice] = 'Teacher was successfully created.'
+        format.html { redirect_to(@teacher) }
+        format.xml  { render :xml => @teacher, :status => :created, :location => @teacher }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @teacher.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def update
+    @teacher = Teacher.find(params[:id])
+
+    respond_to do |format|
+      if @teacher.update_attributes(params[:teacher])
+        flash[:notice] = 'Teacher was successfully updated.'
+        format.html { redirect_to(teachers_url) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @teacher.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @teacher = Teacher.find(params[:id])
+    @teacher.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(teachers_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
