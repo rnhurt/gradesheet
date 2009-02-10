624d4313294575da4597bbb99b1f330b79d2d965
Richard Hurt <rnhurt@kangaroobox.com>
2009-02-10 15:49:55 -0500

    still working on report generation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -17,9 +17,16 @@ class ReportsController < ApplicationController
 		else
 			# Get a list of the cached reports
 			@cached_reports = Report.find_all_by_name(params[:id])
+			
+			# Get a reference to the report cache
+			Dir.mkdir("#{RAILS_ROOT}/tmp/cache/reports/") unless File.exist?("#{RAILS_ROOT}/tmp/cache/reports/")
+			cache_dir = Dir.new("#{RAILS_ROOT}/tmp/cache/reports/")
 
 			respond_to do |format|
-				format.html	# display the parameters screen
+				format.html	do
+					# display the parameters screen
+					@cached_reports = Dir.glob(cache_dir.path + "#{params[:id]}.*")
+				end
 				
 				format.pdf do
 					# Gather all the data we need for the report
@@ -27,15 +34,15 @@ class ReportsController < ApplicationController
 					params[:grades] = Gradation.find_all_by_student_id(params[:student_id])
 					params[:courses] = Course.find_yomama(params[:student_id])
 				
-					# Create a temp file to hold the report cache
-					cache_file = File.new("#{RAILS_ROOT}/tmp/cache/#{params[:id]}.#{Time.now.to_s(:number)}", "w")
+					# Create a file to hold the report cache
+					cache_file = File.new(cache_dir.path + "/#{params[:id]}.#{Time.now.to_s(:number)}", "w")
 					params[:file_name] = cache_file.path
 
 					# Generate the report
 					@report.draw(params)
 										
 					# Send the PDF back to the browser for viewing
-					send_data cache_file.path, 
+					send_file cache_file.path,
 							:filename			=> params[:id].titleize + '.pdf',
 							:type					=> 'application/pdf',
 							:disposition	=> 'inline'
