3f0426ee95d348682d8afa7f33e98a20535c1914
Richard Hurt <rnhurt@kangaroobox.com>
2009-03-20 14:21:45 -0400

    added the ability to add whole classrooms of students to a course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,7 +8,7 @@ class CoursesController < ApplicationController
 	def show
 		# Displays the students enrolled in each course.
     @course = Course.find(params[:id])
-#    @homerooms = Course.find_all_by_course_type_id(CourseType.find_all_by_name('Home Room'))
+    @homerooms = Student.find_homerooms()
 
 		respond_to do |format|
 			format.html
@@ -19,12 +19,10 @@ class CoursesController < ApplicationController
 				# is a better way to do this.
 				value = params[:value].split('||')
 				if value.pop == 'H' then
-					# FIXME
-					# Find students by Home Room
-#					@students = Student.find_by_course(value[0])
-					@students = Student.courses
+					# Find students by "Home Room"
+					@students = Student.find_all_by_homeroom(value[0])
 				else
-					# Find students by Class Of
+					# Find students by "Class Of"
 					@students = Student.find_all_by_class_of(value[0])
 				end
 				render :partial => "student_list"
@@ -97,19 +95,37 @@ class CoursesController < ApplicationController
   end
 
   
- 	## Add a student to a course  
+ 	# Add student(s) to a course  
   def add_student
-		@course = Course.find(params[:id])
-		@student = Student.find(params[:student_id])
-		@add = true
- 		if @course.students.index(@student) == nil
-	    @course.students << @student
-	    @course.save
+		@course = Course.find(params[:id]) 
+
+    # Are we adding one student or an array of students?
+    if params[:students]
+      # Add all the students to the course
+      students = Student.find(params[:students])
+      students.each do |student|
+        begin
+          @course.students << student
+        rescue ActiveRecord::RecordInvalid
+          # This is here to catch an existing student being added to a course
+        end
+      end
+      @course.save
+    else
+      # Add a single student to the course and save it
+  		@student = Student.find(params[:student_id])    	
+  	  @add = true
+		  
+   		if @course.students.index(@student) == nil
+	      @course.students << @student
+	      @course.save
+      end
     end
+    
    	render :action => "modify", :locals => { :add => true }
   end
 
-  ## Remove a student from a course
+  # Remove a student from a course
   def remove_student
  		@course = Course.find(params[:id])
  		@student = Student.find(params[:student_id])
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,6 @@
 class Student < User
 	has_many		:enrollments
 	has_many		:gradations
-#	has_many		:comments
 	has_many		:courses, 		:through	=> :enrollments
 	has_many		:assignments,	:through	=> :gradations
 
@@ -9,16 +8,13 @@ class Student < User
 	
 	from_year = Time.now.year - 1
 	to_year = from_year + 10
-	validates_inclusion_of :class_of, :in => from_year..to_year, 
-												:message => "must be in the range of #{from_year} to #{to_year}"
+	validates_inclusion_of :class_of, 
+	            :in => from_year..to_year, 
+              :message => "must be in the range of #{from_year} to #{to_year}"
 												
-#	named_scope	:course_id, :joins => :enrollments, :conditions => { :id => :student_id }
-
-	# FIXME
 	named_scope	:courses, :joins => :courses
 
 	def self.find_homerooms(*args)
 		return Student.find(:all, :select => 'homeroom', :group => 'homeroom', :conditions => "homeroom > ''").map { |h| h.homeroom }
 	end	
-
 end
