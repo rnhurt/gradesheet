9c89fb02002c68025f498b41533d85ba512cf27e
Richard Hurt <rnhurt@kangaroobox.com>
2009-05-11 12:21:02 -0400

    column_box does not work with tables very well.  Implemented report columns with bounding_box and it seems to be working pretty well.

--- lib/reports/report_card.rb
+++ lib/reports/report_card.rb
@@ -83,14 +83,31 @@ EOS
 
     # Make it so we don't have to use the 'pdf.' prefix everywhere.  :)
     @pdf.instance_eval do 
+      # Generate the table containing the students grade information
+      def make_table(headers, data)
+        table(
+          data,
+          :headers      => headers,
+          :header_color => "C0C0C0",
+          :font_size    => 7,
+          :border_style => :grid,
+          :border_width => 0.5,
+          :width        => bounds.width)
+      end
+
       # Build the header
       header margin_box.top_left do 
         font "Helvetica", :size => 7
-        text "ARCHDIOCESE OF LOUISVILLE", :size => 9
+        text "ARCHDIOCESE OF LOUISVILLE", :align => :center, :size => 11
         text "Report Card", :align => :center, :size => 10
         mask(:y) { text "Grade: 7S", :align => :center }
         text "Student: #{student.full_name}", :align => :left, :size => 10
         stroke_horizontal_rule
+
+        # Reset the column cursor positions whenever we start a new page
+		    left_cursor = cursor - HEADER_HEIGHT
+		    right_cursor = cursor - HEADER_HEIGHT
+
       end
       # Build the footer
       footer margin_box.bottom_left do 
@@ -106,92 +123,90 @@ EOS
         stroke_color "000000"
       end   
 
+      # Loop through each student
 
-      # Print the grading keys
-      # FIXME: There is probably a more "Ruby" way to do this...
-      scales = Array.new
-      student.courses.each { |course| scales.push(course.grading_scale) }
-      ReportCard.print_keys(scales)
+        # Print the grading keys
+        # FIXME: There is probably a more "Ruby" way to do this...
+        scales = Array.new
+        student.courses.each { |course| scales.push(course.grading_scale) }
+        ReportCard.print_keys(scales)
 
-      # Set up the text options
-      font "Helvetica"
-      text_options.update(:size => 7, :align => :left)
-      
-  	  # Print the grades for each class
-  	  student.courses.each_with_index do |course, index|
-  		  # Build the header and data information for this course
-  		  headers = ["#{course.name} - #{course.term.name}\n  #{course.teacher.full_name}", "A(93%)", "B(86%)", "n/a", "B(90%)"]
-  		  data = [
-  			  ["Application","A","B"," ","B"],
-  			  ["Test/quizes","B","B"," ","B"],
-  			  ["Test/quizes","B","B"," ","B"],
-  			  ["Test/quizes","B","B"," ","B"],
-  			  ["Class participation","C","C"," ","C"],
-  			  ["Projects/activities","A","A"," ","A"],
-  			  ["Homework","C","C"," ","C"],
-  			  ["Work ethic","C","C"," ","C"],
-  			  ["Behavior","C","C"," ","C"],
-  			  [" "," "," "," "," "],
-  			  [" "," "," "," "," "]
-  		  ]
-
-        column_box [0,cursor], :width => bounds.width, :columns => 2 do
-          text " This is a test " * 100
-          table data, :headers => headers,
-            :header_color => 'C0C0C0',
-            :font_size    => 7,
-            :border_style => :grid,
-            :border_width => 0.5,
-            :width        => bounds.width-10
-        end
+        # Set up the text options
+        font "Helvetica"
+        text_options.update(:size => 7, :align => :left)
         
-        move_down BUFFER_SIZE
-  
-#  		  # Print half of the courses in the left column and the other half in the right
-#  		  if index.even?
-#  			  mask (:y) {
-#  				  span((bounds.width/2) - BUFFER_SIZE, :position => :left) do
-#  					  table data, :headers => headers,
-#  						  :header_color => "C0C0C0",
-#  						  :font_size	=> 7,
-#  						  :border_style	=> :grid,
-#  						  :border_width	=> 0.5,
-#  						  :width	=> bounds.width-10
-#  				  end
-#  		    }
-#  		  else
-#          data.pop  
-#          data.pop  
-#          data.pop  
-#  			  span((bounds.width/2) - BUFFER_SIZE, :position => :right) do
-#  				  table data, :headers => headers,
-#  					  :header_color => "C0C0C0",
-#  					  :font_size	=> 7,
-#  					  :border_style	=> :grid,
-#  					  :border_width	=> 0.5,
-#  					  :width	=> bounds.width
-#  			  end
-#  		  end
-#      		
-#  	  # Try not to overflow into the next page
-#  	  if cursor < 200
-#  		  start_new_page
-#  		  move_down HEADER_HEIGHT 		# make room for the header
-#  	  end
-  	
-  	  end	# each course
+        left_cursor = nil
+        right_cursor = nil
+        
+    	  # Print the grades for each class
+    	  student.courses.each_with_index do |course, index|
+
+    	    # Try not to overflow into the next page
+	        if cursor < 300
+		        start_new_page
+		        left_cursor = cursor - HEADER_HEIGHT
+		        right_cursor = cursor - HEADER_HEIGHT
+		        move_down HEADER_HEIGHT 		# make room for the header
+	        end
+
+    		  # Build the header and data information for this course
+    		  headers = ["#{course.name} - #{course.term.name}\n  #{course.teacher.full_name}", "A(93%)", "B(86%)", "n/a", "B(90%)"]
+    		  data = [
+    			  ["Application","A","B"," ","B"],
+    			  ["Test/quizes","B","B"," ","B"],
+    			  ["Test/quizes","B","B"," ","B"],
+    			  ["Test/quizes","B","B"," ","B"],
+    			  ["Class participation","C","C"," ","C"],
+    			  ["Projects/activities","A","A"," ","A"],
+    			  ["Homework","C","C"," ","C"],
+    			  ["Work ethic","C","C"," ","C"],
+    			  ["Behavior","C","C"," ","C"],
+    			  ["Work ethic","C","C"," ","C"],
+    			  ["Behavior","C","C"," ","C"],
+    			  ["Work ethic","C","C"," ","C"],
+    			  ["Behavior","C","C"," ","C"],
+    			  ["Work ethic","C","C"," ","C"],
+    			  ["Behavior","C","C"," ","C"],
+    			  ["Work ethic","C","C"," ","C"],
+    			  ["Behavior","C","C"," ","C"],
+    			  [" "," "," "," "," "],
+    			  [" "," "," "," "," "]
+    		  ]
+          
+          left_cursor ||= cursor
+          right_cursor ||= cursor
+
+  #      rand(8).times { data.pop }     # Test for different column heights
+          
+          bounding_box([0, left_cursor], :width => (bounds.width / 2) - BUFFER_SIZE) do
+            make_table(headers,data)
+          end
+          move_down(BUFFER_SIZE)
+          left_cursor = cursor
+
+  #      rand(8).times { data.pop }     # Test for different column heights
+
+          bounding_box([bounds.left + bounds.width / 2, right_cursor], :width => (bounds.width / 2) - BUFFER_SIZE) do
+            make_table(headers,data)
+          end
+          move_down(BUFFER_SIZE)
+          right_cursor = cursor
+            	
+      end	# each course
 
       # Always print an even number of pages.  This prevents the start of one
       # report from printing on the back of the previous report.
       start_new_page unless page_count.even?
-    end # instance_eval
+      end # instance_eval
     
-    
+    end # each student
     # Render the document
 	  @pdf.render
 	end
 
-	
+
+
+  	
 	# Print the grading key for a specified course
 	def self.print_keys(scales)
 	  # Check for something to print...
@@ -201,10 +216,10 @@ EOS
 	  scales.uniq!
       
     @pdf.instance_eval do    
-      bounding_box [0, cursor - HEADER_HEIGHT], :width => bounds.width  do
+      bounding_box [0, cursor - HEADER_HEIGHT], :width => bounds.width do
         text "Grading Scales", :size => 10
-        stroke_bounds
-      
+        stroke_horizontal_rule
+
         # Set up the text options
   	    font "Helvetica"
   	    text_options.update(:size => 7, :align => :left)
@@ -220,7 +235,7 @@ EOS
           scale.grade_ranges.each_with_index do |range, index|
             if index.even?
               mask(:y) {
-                span(bounds.width/2-BUFFER_SIZE, :position => :left) do
+                span(bounds.width/2 - BUFFER_SIZE, :position => :left) do
                   text "  #{range.grade} - #{range.description} (#{range.min_score}% and above)"            
                 end
               }
@@ -231,7 +246,7 @@ EOS
             end
           end
           
-          move_down BUFFER_SIZE
+          move_down BUFFER_SIZE + 10
         end
       end
 
