27363d5131092406f75b8bff8ce71ae9829f2527
Richard Hurt <rnhurt@kangaroobox.com>
2009-05-14 15:18:31 -0400

    Report Card is now duplexing properly.

--- lib/reports/report_card.rb
+++ lib/reports/report_card.rb
@@ -6,7 +6,7 @@ require "date"
 class ReportCard
 
   HEADER_HEIGHT = 50  # The size of the page header
-  BUFFER_SIZE   = 5   # The size of the buffer between elements
+  GUTTER_SIZE   = 5   # The size of the buffer between elements
   
   # Build the parameter window to be shown to the user.	
 	def self.get_params()
@@ -76,50 +76,62 @@ EOS
 	def self.draw(params)
 	  # Gather the required data from the database
 	  term = Term.find(params[:term_id])
-	  
-	  if params.key?(:student_id) then
-	    # Search for individual student(s)
-  	  students = Student.all(:conditions => params[:student_id])
-  	else
+
+	  if params[:student_id].nil? then
   	  # Process a whole homeroom
   	  students = Student.find_all_by_homeroom(params[:homeroom_id])
+  	else
+	    # Search for individual student(s)
+  	  students = Student.find(:all, :conditions => { :id => params[:student_id]})
   	end
 
     # Create a new document
     @pdf = Prawn::Document.new :page_size => "LEGAL", :skip_page_creation => false
 
     # Make it so we don't have to use the 'pdf.' prefix everywhere.  :)
-    @pdf.instance_eval do 
-      # Loop through each student
-      students.each do |student|
-    
-        # Generate the table containing the students grade information
-        def make_table(headers, data)
-          table(
-            data,
-            :headers      => headers,
-            :header_color => "C0C0C0",
-            :font_size    => 7,
-            :border_style => :grid,
-            :border_width => 0.5,
-            :width        => bounds.width)
-        end
+    @pdf.instance_eval do
+      @initial_cursor = cursor  # Use this to reset the position on each new page
 
-        # Build the header
+      # Generate the table containing the course grade information
+      def make_table(headers, data)
+        table(
+          data,
+          :headers      => headers,
+          :header_color => "C0C0C0",
+          :font_size    => 7,
+          :border_style => :grid,
+          :border_width => 0.5,
+          :width        => bounds.width)
+      end
+
+      # Generate a new page for the report.
+      def new_page
+        # Reset the column cursor positions whenever we start a new page
+        @left_cursor = @initial_cursor - HEADER_HEIGHT
+        @right_cursor = @initial_cursor - HEADER_HEIGHT
+        
+        # Trigger a new page
+        start_new_page          
+      end
+      
+      # Loop through each student
+      students.each_with_index do |student, sindex|
+
+        # Count the number of pages each student requires so that we can duplex properly
+        student_page_count = 0
+                  
+        # Build the page header
         header margin_box.top_left do 
           font "Helvetica", :size => 7
           text "ARCHDIOCESE OF LOUISVILLE", :align => :center, :size => 11
           text "Report Card", :align => :center, :size => 10
-          mask(:y) { text "Grade: 7S", :align => :center }
+          mask(:y) { text "Homeroom: #{student.homeroom}", :align => :center }
           text "Student: #{student.full_name}", :align => :left, :size => 10
           stroke_horizontal_rule
-
-          # Reset the column cursor positions whenever we start a new page
-		      left_cursor = cursor - HEADER_HEIGHT
-		      right_cursor = cursor - HEADER_HEIGHT
-
+          
+          student_page_count += 1
         end
-        # Build the footer
+        # Build the page footer
         footer margin_box.bottom_left do 
           font "Helvetica", :size => 7
           fill_color "555555"
@@ -139,24 +151,19 @@ EOS
         student.courses.each { |course| scales.push(course.grading_scale) }
         ReportCard.print_keys(scales)
 
+        # Set the default column cursor position
+        @left_cursor = cursor
+        @right_cursor = cursor
+
         # Set up the text options
         font "Helvetica"
         text_options.update(:size => 7, :align => :left)
-        
-        left_cursor = nil
-        right_cursor = nil
-        student_page_count = 0
         
     	  # Print the grades for each class
     	  student.courses.each_with_index do |course, index|
 
     	    # Try not to overflow into the next page
-	        if cursor < 300
-		        start_new_page
-		        left_cursor = cursor - HEADER_HEIGHT
-		        right_cursor = cursor - HEADER_HEIGHT
-		        move_down HEADER_HEIGHT 		# make room for the header
-	        end
+	        new_page if cursor < 300
 
     		  # Build the header and data information for this course
     		  headers = ["#{course.name} - #{course.term.name}\n  #{course.teacher.full_name}", "A(93%)", "B(86%)", "n/a", "B(90%)"]
@@ -181,42 +188,46 @@ EOS
     			  [" "," "," "," "," "],
     			  [" "," "," "," "," "]
     		  ]
-          
-          left_cursor ||= cursor
-          right_cursor ||= cursor
 
-  #      rand(8).times { data.pop }     # Test for different column heights
-          
-          bounding_box([0, left_cursor], :width => (bounds.width / 2) - BUFFER_SIZE) do
-            make_table(headers,data)
+
+          # Print the course data alternately on the left & right side of the page
+          if index.even? then
+            bounding_box([0, @left_cursor], :width => (bounds.width / 2) - GUTTER_SIZE) do
+              make_table(headers,data)
+            end
+            move_down(GUTTER_SIZE)
+            @left_cursor = cursor
+          else
+            bounding_box([bounds.left + bounds.width / 2, @right_cursor], :width => (bounds.width / 2) - GUTTER_SIZE) do
+              make_table(headers,data)
+            end
+            move_down(GUTTER_SIZE)
+            @right_cursor = cursor
           end
-          move_down(BUFFER_SIZE)
-          left_cursor = cursor
 
-  #      rand(8).times { data.pop }     # Test for different column heights
-
-          bounding_box([bounds.left + bounds.width / 2, right_cursor], :width => (bounds.width / 2) - BUFFER_SIZE) do
-            make_table(headers,data)
-          end
-          move_down(BUFFER_SIZE)
-          right_cursor = cursor
-          
         end	# each course
 
-      # Always print an even number of pages for a student.  This prevents the
-      # start of one report from printing on the back of the previous report.
-      start_new_page unless student_page_count.even?
-      
+        # Is this the last student?
+        if sindex < students.count - 1 then
+          # No, force a page break between students
+          new_page
+        else
+          # Yes, make sure we don't add a page break unnecessarily
+          student_page_count = 0
+        end
+
+        # Always print an even number of pages for a student.  This prevents the
+        # start of one report from printing on the back of the previous report.
+        new_page unless student_page_count.even?
+        
       end # each student
+                  
     end # instance_eval
     
     
     # Render the document
 	  @pdf.render
 	end
-
-
-
   	
 	# Print the grading key for a specified course
 	def self.print_keys(scales)
@@ -237,7 +248,7 @@ EOS
 
         scales.each do |scale|
           # Print the grading scale header
-          move_down BUFFER_SIZE
+          move_down GUTTER_SIZE
           text "#{scale.name}", :size => 8
           stroke_horizontal_rule
           move_down 2
@@ -246,22 +257,22 @@ EOS
           scale.grade_ranges.each_with_index do |range, index|
             if index.even?
               mask(:y) {
-                span(bounds.width/2 - BUFFER_SIZE, :position => :left) do
+                span(bounds.width/2 - GUTTER_SIZE, :position => :left) do
                   text "  #{range.grade} - #{range.description} (#{range.min_score}% and above)"            
                 end
               }
             else
-              span((bounds.width/2) - BUFFER_SIZE, :position => :right) do
+              span((bounds.width/2) - GUTTER_SIZE, :position => :right) do
                 text "  #{range.grade} - #{range.description} (#{range.min_score}% and above)"
               end
             end
           end
           
-          move_down BUFFER_SIZE + 10
+          move_down GUTTER_SIZE + 10
         end
       end
 
-      move_down BUFFER_SIZE
+      move_down GUTTER_SIZE
 		end # instance_eval
 
 		
