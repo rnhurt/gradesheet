b85511d150ad0a0c66974813b69ac9f226c0cccc
Richard Hurt <rnhurt@kangaroobox.com>
2009-05-27 11:25:16 -0400

    Moved Prawn to vendor/gems

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,10 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   config.load_paths += %W( #{RAILS_ROOT}/lib/reports )
 
+  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
+    File.directory?(lib = "#{dir}/lib") ? lib : dir
+  end
+
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
