f2c6654553051ea5ba378e9a42babba6c9fae00e
Richard Hurt <rnhurt@kangaroobox.com>
2009-08-26 14:46:15 -0400

    Grading Scale Range correction  * GradingScale ranges are now checked for validity  * Redesigned the grading scale edit page to make it more clear and pretty  * Slight tweak to the school year edit page to make it more pretty

--- app/models/scale_range.rb
+++ app/models/scale_range.rb
@@ -5,12 +5,18 @@ class ScaleRange < ActiveRecord::Base
   validates_length_of       :description, :within => 0..250
   validates_length_of       :letter_grade, :within => 1..20
 
-  validates_numericality_of :max_score, :greater_than_or_equal_to => 0, :allow_nil => true
-  validates_numericality_of :min_score, :greater_than_or_equal_to => 0, :allow_nil => true
-
-#	validates_existence_of    :grading_scale
+  validates_numericality_of :max_score, :greater_than_or_equal_to => 0
+  validates_numericality_of :min_score, :greater_than_or_equal_to => 0
 
   attr_accessor :should_destroy
+
+  def before_validation
+    if min_score >= max_score
+      self.errors.add_to_base("Minimum Score cannot be greater than Maximum Score")
+      return false 
+    end
+
+  end
 
   # This attribute is set in the scale_range partial when the user wants to 
   # remove an existing grade range.
