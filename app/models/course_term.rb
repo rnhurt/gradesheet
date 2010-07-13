363109f75f304bee6738d28cfb06bedc99923c0a
Richard Hurt <rnhurt@kangaroobox.com>
2010-07-13 13:07:32 -0400

    Converted to Rails 2.3.5 (finally)

--- app/models/course_term.rb
+++ app/models/course_term.rb
@@ -18,27 +18,42 @@ class CourseTerm < ActiveRecord::Base
   delegate :grading_scale,  :to => :course
   delegate :students,       :to => :course
 
+  named_scope :active, :include => :term,
+    :conditions	=> ["date_ranges.active = ?", true]
+
+  named_scope :sorted, :order => "date_ranges.end_date ASC"
+
   # Calculate a students current grade for a particular course & term.
   def calculate_grade(student_id)
     # Set up some variables
-    points_earned       = 0.0
-    possible_points     = 0.0
+    points_earned   = 0.0
+    possible_points = 0.0
 
-    # Loop through the assignments for computing the grade as we go
-    self.assignment_evaluations.all(:conditions => { :student_id => student_id}).each do |evaluation|
-      valid_points = evaluation.points_earned_as_number
-      if valid_points
-        points_earned += valid_points.to_f
-        possible_points += evaluation.assignment.possible_points.to_f
+    cached_points = Rails.cache.read("#{self.id}|#{student_id}")
+
+    # FIXME: I'm not sure this is what I want here.
+    if !cached_points
+
+      # Loop through the assignments for computing the grade as we go
+      self.assignment_evaluations.all(:conditions => { :student_id => student_id}).each do |evaluation|
+        valid_points = evaluation.points_earned_as_number
+        if valid_points
+          points_earned += valid_points.to_f
+          possible_points += evaluation.assignment.possible_points.to_f
+        end
+        logger.debug " **** points earned: #{valid_points} out of #{evaluation.assignment.possible_points}"
       end
-      logger.debug " **** points earned: #{valid_points} out of #{evaluation.assignment.possible_points}"
+
+      logger.debug  " **** final! #{points_earned} out of #{possible_points}"
+      # Sanitize the score & grade so that we don't try to divide by zero or anything stupid
+      final_score = possible_points > 0 ? ((points_earned/possible_points)*100).round(2) : -1
+
+      Rails.cache.write("#{self.id}|#{student_id}", final_score)
+    else
+      final_score = cached_points
     end
-
-    logger.debug  " **** final! #{points_earned} out of #{possible_points}"
-    # Sanitize the score & grade so that we don't try to divide by zero or anything stupid
-    final_score = possible_points > 0 ? ((points_earned/possible_points)*100).round(2) : -1
+    
     letter_grade = final_score > 0 ? self.course.grading_scale.calculate_letter_grade(final_score) : 'n/a'
-
     return {:letter => letter_grade, :score => final_score }
   end
 
