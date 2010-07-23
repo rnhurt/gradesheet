95c4cff04c3b1c6d4727fc957f118504ec1f82ca
Richard Hurt <rnhurt@kangaroobox.com>
2010-07-22 22:10:45 -0400

    WIP: performance tweaks

--- db/migrate/025_add_indexes_1.rb
+++ db/migrate/025_add_indexes_1.rb
@@ -3,11 +3,20 @@ class AddIndexes1 < ActiveRecord::Migration
     add_index :assignments, [:course_term_id]
     add_index :assignment_evaluations, [:student_id]
     add_index :assignment_evaluations, [:assignment_id]
+
+    # Update an index to include 'class_of'
+		remove_index :users, [:first_name, :last_name]
+		add_index :users, [:first_name, :last_name, :class_of], :unique => true
+
   end
 
   def self.down
     remove_index :assignments, [:course_term_id]
     remove_index :assignment_evaluations, [:student_id]
     remove_index :assignment_evaluations, [:assignment_id]
+
+    # Update an index to remove 'class_of'
+ 		remove_index :users, [:first_name, :last_name, :class_of]
+		add_index :users, [:first_name, :last_name], :unique => true
   end
 end
