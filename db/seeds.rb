9ed07f5da2b186e34a22f163ebe78d20d0b20f23
Richard Hurt <rnhurt@kangaroobox.com>
2010-11-06 21:12:29 -0400

    Merged commit from 'https://github.com/ccabot/gradesheet'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
-s = Site.find_or_create_by_name 'seed site'
-User.find_or_create_by_login :login => 'admin', :site => s, :password => 'password', :password_confirmation => 'password', :first_name => 'seed', :last_name => 'user', :is_admin => true
+# Create a new default site
+site = Site.find_or_create_by_name 'Main'
+
+# Add the admin user
+User.find_or_create_by_login :login => 'admin', :site => site,
+  :password => 'password', :password_confirmation => 'password',
+  :first_name => 'admin', :last_name => 'user', :is_admin => true
