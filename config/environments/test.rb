fe48a58a67e1396877cebaf57ea4fafb905093fc
Richard Hurt <rnhurt@kangaroobox.com>
2010-11-07 21:29:31 -0500

    test are back to 100% passing.  Man, I'm beginning to hate fixtures.  :(

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,6 +27,6 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+config.gem "factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
 config.gem "shoulda", :lib => "shoulda"
 
